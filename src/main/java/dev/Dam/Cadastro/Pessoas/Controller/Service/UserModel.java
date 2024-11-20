3ee1a7fc87bbc0c2249acc8a0ee3a2621aca9754
Dalton Dam <daltondam26@gmail.com>
2024-11-20 16:31:48 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/dev/Dam/Cadastro/Pessoas/Controller/Service/UserModel.java
+++ src/main/java/dev/Dam/Cadastro/Pessoas/Controller/Service/UserModel.java
@@ -2,6 +2,9 @@ package dev.Dam.Cadastro.Pessoas.Controller.Service;
 
 import dev.Dam.Cadastro.Tasks.TasksModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -9,50 +12,24 @@ import java.util.List;
 //JPA = Java Persistence API
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor //Lombok cria o construtor nas espreitas
+@AllArgsConstructor
+@Data //Todos os getters e os setters
 public class UserModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private int age;
 
     //@ManyToOne - O usuário só pode ter uma única task
     @ManyToOne
-    @JoinColumn(name = "tasks_id") //Foreing key ou chave es
+    @JoinColumn(name = "tasks_id") //Foreing key ou chave e strangeira
     private TasksModel tasks;
 
-    public UserModel() {
-    }
 
-    public UserModel(String name, String email, int age) {
-        this.name = name;
-        this.email = email;
-        this.age = age;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
--- src/main/java/dev/Dam/Cadastro/Tasks/TasksModel.java
+++ src/main/java/dev/Dam/Cadastro/Tasks/TasksModel.java
@@ -2,11 +2,17 @@ package dev.Dam.Cadastro.Tasks;
 
 import dev.Dam.Cadastro.Pessoas.Controller.Service.UserModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_Tasks")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class TasksModel {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
