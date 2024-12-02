aa005f09f73664a0de5fb4b323d26bb4a8747a1e
Dalton Dam <daltondam26@gmail.com>
2024-12-02 19:56:36 -0300

    refactor: Inclui a annotation toString nos models de User e Tasks

--- src/main/java/dev/Dam/Cadastro/Pessoas/Controller/Service/UserModel.java
+++ src/main/java/dev/Dam/Cadastro/Pessoas/Controller/Service/UserModel.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ import java.util.List;
 @Table(name = "tb_cadastro")
 @NoArgsConstructor //Lombok cria o construtor nas espreitas
 @AllArgsConstructor
+@ToString(exclude = "tasks")
 @Data //Todos os getters e os setters
 public class UserModel {
 
